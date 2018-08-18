e48090286d7d3e5e79374dfa34df9395c42ad45b
laffingragons <akshaypatil,vit@gmail.com>
2018-08-19 01:15:32 +0530

    prod

--- config/appConfig.js
+++ config/appConfig.js
@@ -4,7 +4,8 @@ appConfig.port = 3000;
 appConfig.allowedCorsOrigin = "*";
 appConfig.env = "dev";
 appConfig.db = {
-    uri: "mongodb://127.0.0.1:27017/candyShopApi"
+    // uri: "mongodb://127.0.0.1:27017/candyShopApi"
+    uri:"mongodb://<dbuser>:<dbpassword>@ds125472.mlab.com:25472/candy-shop"
 }
 appConfig.apiVersion = "/api/v1";
 
